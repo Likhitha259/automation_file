e402512b5d75aab759ec7861bad549f2258c85c5
Likhitha <likhithaygramesh259@gmail.com>
2026-04-09 07:31:35 +0530

    changes from dummy branch

---
+++ pages/base_page.py
@@ -0,0 +1,16 @@
+class BasePage:
+    def __init__(self,driver):
+        self.driver=driver
+
+    def send_keys(self,locator,text):
+        '''this methods takes locator and text'''
+        ele1=self.driver.find_element(*locator)  #*locator=loc_name,loc_value
+        ele1.clear()
+        ele1.send_keys(text)
+
+    def click(self,locator):
+        ele2=self.driver.find_element(*locator)
+        ele2.click()
+
+    print('hai hello')
+    print('this is from dummy branch')
